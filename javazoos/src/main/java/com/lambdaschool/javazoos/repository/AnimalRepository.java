d1a69818a7179da46df3a21fd98c47a02973edbc
ginabethrussell <ginabeth.russell@gmail.com>
2021-02-18 18:11:58 -0500

    add route for custom query of animal zoo count

--- javazoos/src/main/java/com/lambdaschool/javazoos/repository/AnimalRepository.java
+++ javazoos/src/main/java/com/lambdaschool/javazoos/repository/AnimalRepository.java
@@ -1,8 +1,21 @@
 package com.lambdaschool.javazoos.repository;
 
 import com.lambdaschool.javazoos.models.Animal;
+import com.lambdaschool.javazoos.views.AnimalZooCount;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface AnimalRepository extends CrudRepository<Animal, Long>
 {
+
+    @Query(value="SELECT a.animaltype AS animaltype, a.animalid AS animalid, count(a.animalid) AS countzoos " +
+        "FROM animals a LEFT JOIN zooanimals z " +
+        "ON a.animalid = z.animalid " +
+        "GROUP BY a.animalid " +
+        "ORDER BY countzoos DESC", nativeQuery = true)
+    List<AnimalZooCount> getAnimalZooList();
+
+
 }
--- javazoos/src/main/java/com/lambdaschool/javazoos/services/AnimalServiceImpl.java
+++ javazoos/src/main/java/com/lambdaschool/javazoos/services/AnimalServiceImpl.java
@@ -1,9 +1,12 @@
 package com.lambdaschool.javazoos.services;
 
 import com.lambdaschool.javazoos.repository.AnimalRepository;
+import com.lambdaschool.javazoos.views.AnimalZooCount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional
 @Service(value="animalService")
@@ -11,4 +14,11 @@ public class AnimalServiceImpl implements AnimalService
 {
     @Autowired
     AnimalRepository animalRepository;
+
+    @Override
+    public List<AnimalZooCount> getAnimalZooList()
+    {
+       List<AnimalZooCount> animalZooCountList = animalRepository.getAnimalZooList();
+       return animalZooCountList;
+    }
 }
